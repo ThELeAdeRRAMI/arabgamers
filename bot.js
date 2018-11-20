41d809e34def6e2555e223c611bb771e6e582ff0
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-20 16:05:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -271,7 +271,7 @@ client.on('message', message => {
     }
 });
 
-const developers = ["463450251525750796"]
+const developers = ["340489542236766208"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
